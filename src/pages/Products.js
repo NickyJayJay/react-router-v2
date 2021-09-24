c8ec1560cc0ff9b909ed8fca5a2f2c75c2791154
Nick Jacob <njacob341@gmail.com>
2021-09-24 16:38:03 -0700

    Adding dynamic routes with params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,14 @@
 const Products = () => {
-    return <h1>The Products Page</h1>;
-  };
-  
-  export default Products;
-  
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpete</li>
+        <li>An Online Course</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
